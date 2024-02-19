14c4bf4e1abc4d2c8ecc7f65d6f5facf40f1688f
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-02-18 22:34:47 -0300

    v4.0

--- src/utils/generateTimer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateTimer = void 0;
-function generateTimer(min, max) {
-    var numSal = Math.random();
-    var numeroAleatorio = Math.floor(numSal * (max - min + 1)) + min;
-    return numeroAleatorio;
-}
-exports.generateTimer = generateTimer;
