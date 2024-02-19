7284ffb6a2e287bffe752659f4712dee7ebb520b
Smarter ChatBOT <smarterbotcl@gmail.com>
2024-02-18 22:12:46 -0300

    v2.2

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,8 @@ import { createBot, MemoryDB, createProvider } from '@bot-whatsapp/bot'
 import { TelegramProvider } from '@builderbot-plugins/telegram'
 import { BaileysProvider } from '@bot-whatsapp/provider-baileys'
 
-import AIClass from './services/ai';
+import { AIClass } from './services/ai';
+
 import flows from './flows';
 
 const ai = new AIClass(process.env.OPEN_API_KEY, 'gpt-3.5-turbo-16k')
